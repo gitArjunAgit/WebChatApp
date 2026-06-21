9fcfabbce7a13cd8e7f1e2d4dfe557f712bcee0e
Arj <arjunfiles@thearjunfiles.local>
2026-06-21 12:55:44 -0400

    added mongodb permanent storage

--- app.py
+++ app.py
@@ -1,32 +1,50 @@
 import os
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
+from pymongo import MongoClient
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-chat_history = []
+# Connect to MongoDB using an Environment Variable
+MONGO_URI = os.environ.get("MONGO_URI")
+
+# We use a fallback to memory just in case the database isn't hooked up yet
+if MONGO_URI:
+    client = MongoClient(MONGO_URI)
+    db = client['arj_domain']
+    messages_collection = db['messages']
+else:
+    messages_collection = None
+    chat_history = []
+
 typing_users = set()
-# Track active users by mapping session IDs to user details
 active_users = {}
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @socketio.on('connect')
 def handle_connect():
-    emit('load_history', chat_history)
+    # Load history from MongoDB, or fallback to local memory
+    if messages_collection is not None:
+        history = list(messages_collection.find({}, {'_id': 0}))
+    else:
+        history = chat_history
+    emit('load_history', history)
+
 
 @socketio.on('join_domain')
 def handle_join(data):
-    # Register user session details
     active_users[request.sid] = {"user": data['user'], "color": data['color']}
     emit('update_users', list(active_users.values()), broadcast=True)
 
+
 @socketio.on('disconnect')
 def handle_disconnect():
-    # Remove user if they exit or close the tab
     if request.sid in active_users:
         user_data = active_users[request.sid]
         if user_data['user'] in typing_users:
@@ -35,24 +53,39 @@ def handle_disconnect():
         del active_users[request.sid]
     emit('update_users', list(active_users.values()), broadcast=True)
 
+
 @socketio.on('send_message')
 def handle_send_message(data):
-    chat_history.append(data)
+    # Save message to database permanently
+    if messages_collection is not None:
+        messages_collection.insert_one(data.copy())
+    else:
+        chat_history.append(data)
+
     if data['user'] in typing_users:
         typing_users.remove(data['user'])
         emit('update_typing', list(typing_users), broadcast=True)
+
     emit('receive_message', data, broadcast=True)
+
 
 @socketio.on('clear_chat')
 def handle_clear_chat():
-    global chat_history
-    chat_history = []
+    # Wipe the database
+    if messages_collection is not None:
+        messages_collection.delete_many({})
+    else:
+        global chat_history
+        chat_history = []
+
     emit('chat_cleared', broadcast=True)
+
 
 @socketio.on('typing')
 def handle_typing(data):
     typing_users.add(data['user'])
     emit('update_typing', list(typing_users), broadcast=True)
+
 
 @socketio.on('stop_typing')
 def handle_stop_typing(data):
@@ -60,9 +93,11 @@ def handle_stop_typing(data):
         typing_users.remove(data['user'])
     emit('update_typing', list(typing_users), broadcast=True)
 
+
 @socketio.on('ping')
 def handle_ping():
     pass
+
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
